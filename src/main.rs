59c33754d26eda11a03a7021f0aa5fdb1f8b0cbc
haghir <47360153+haghir@users.noreply.github.com>
2023-03-28 21:53:20 +0900

    Improve locking the Mutex object.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,56 @@
 use actix_web::{web::Data, get, App, HttpServer, HttpResponse, Result};
 use actix_web_httpauth::extractors::basic::BasicAuth;
-use mysql_async::{prelude::*, Opts, OptsBuilder, Pool};
-use redis::{Client, AsyncCommands};
+use mysql_async::{prelude::*, Opts, OptsBuilder, Pool as MySQLPool};
+use redis::{Client as RedisClient, AsyncCommands};
 use sha2::{Sha256, Digest};
 use std::{env, sync::Mutex};
 use log::{info, debug};
+
+struct CredContext {
+    // MySQL connection pool
+    mysql: Option<Box<MySQLPool>>,
+
+    // Redis client
+    redis: Option<Box<RedisClient>>,
+}
+
+impl CredContext {
+    async fn close(&mut self) {
+        let mysql_pool = std::mem::replace(&mut self.mysql, None);
+        mysql_pool.unwrap().disconnect().await.unwrap();
+    }
+}
 
 fn get_enval(name: &str, default: &str) -> String {
     match env::var(name) {
         Ok(val) => val,
         Err(_) => default.to_string(),
     }
+}
+
+fn get_enval_usize(name: &str, default: &str) -> usize {
+    get_enval(name, default).parse().unwrap()
+}
+
+fn new_mysql_pool() -> Box<MySQLPool> {
+    let mysql_host = get_enval("MYSQL_HOST", "127.0.0.1");
+    let mysql_port = get_enval("MYSQL_PORT", "3306").parse::<u16>().unwrap();
+    let mysql_username = get_enval("MYSQL_USERNAME", "minauth"); // default for debug
+    let mysql_password = get_enval("MYSQL_PASSWORD", "minauth"); // default for debug
+    let mysql_dbname = get_enval("MYSQL_DBNAME", "minauth");
+    let mysql_opts = OptsBuilder::default()
+        .ip_or_hostname(mysql_host)
+        .tcp_port(mysql_port)
+        .user(Some(mysql_username))
+        .pass(Some(mysql_password))
+        .db_name(Some(mysql_dbname))
+        .into();
+    Box::new(MySQLPool::new::<Opts>(mysql_opts))
+}
+
+fn new_redis_client() -> Box<RedisClient> {
+    let redis_uri = get_enval("REDIS_URI", "redis://127.0.0.1/");
+    Box::new(RedisClient::open(redis_uri.as_str()).unwrap())
 }
 
 fn get_hash(password: &str) -> String {
@@ -21,25 +61,31 @@ fn get_hash(password: &str) -> String {
 }
 
 async fn verify(
-    mysql_pool: &mut Box<Pool>,
-    redis_cli: &Client,
+    cred_ctx: &mut CredContext,
     redis_lifetime: usize,
     secret: &String,
     user_id: &str,
     password: &str
 ) -> bool {
-    let mut myconn = mysql_pool.as_mut().get_conn().await.unwrap();
-    let mut redconn = redis_cli.get_async_connection().await.unwrap();
+    // Retrieve a MySQL connection from the connection pool.
+    let myconn = cred_ctx.mysql.as_mut().unwrap();
+    let mut myconn = myconn.get_conn().await.unwrap();
 
+    // Retrieve an async connection of Redis.
+    let redconn = cred_ctx.redis.as_mut().unwrap();
+    let mut redconn = redconn.get_async_connection().await.unwrap();
+
+    // Calculate a SHA256 digest of a string concatenated the secret and the password.
     let password = format!("{}{}", secret, password);
-    let pwhash = get_hash(password.as_str()).to_lowercase();
-    debug!("Authenticating '{}' with {}.", user_id, pwhash);
+    let target = get_hash(password.as_str()).to_lowercase();
+    debug!("Authenticating '{}' with {}.", user_id, target);
 
     // If the credential information for the user is cached,
     // it is not necessary to check the record in MySQL.
     if let Ok(cached) = redconn.get::<&str, String>(user_id).await {
-        debug!("A cached credential was found.");
-        return if pwhash.eq(&cached) {
+        debug!("A cached credential information was found.");
+
+        return if target.eq(&cached) {
             debug!("Succeeded.");
             true
         } else {
@@ -48,45 +94,41 @@ async fn verify(
         }
     }
 
-    let selected = {
-        let pwhash = pwhash.clone();
+    // Retrieve the credential information from the MySQL database.
+    let pwhash: Option<String> = "SELECT pwhash FROM credentials WHERE id = :id"
+        .with(params! { "id" => user_id })
+        .first(&mut myconn)
+        .await
+        .unwrap();
 
-        "SELECT 1 FROM credentials WHERE id = :id AND pwhash = :pwhash"
-            .with(params! {
-            "id" => user_id,
-            "pwhash" => pwhash,
-        })
-            .map(&mut myconn, |x: u8| x)
-            .await
-            .unwrap()
-    };
+    if let Some(pwhash) = pwhash {
+        let _: () = redconn.set_ex(user_id, pwhash.as_str(), redis_lifetime).await.unwrap();
 
-    for _ in selected {
-        debug!("Succeeded.");
-        let _: () = redconn.set_ex(
-            user_id, pwhash.as_str(), redis_lifetime).await.unwrap();
-        return true;
+        if target.eq(&pwhash) {
+            debug!("Succeeded.");
+            true
+        } else {
+            debug!("Failed.");
+            false
+        }
+    } else {
+        let _: () = redconn.set_ex(user_id, "", redis_lifetime).await.unwrap();
+        debug!("Failed.");
+        false
     }
-
-    debug!("Failed.");
-    false
 }
 
 #[get("/auth")]
 async fn auth(
-    mysql_pool: Data<Mutex<Option<Box<Pool>>>>,
-    redis_cli: Data<Mutex<Client>>,
+    cred_ctx: Data<Mutex<CredContext>>,
     redis_lifetime: Data<usize>,
     secret: Data<String>,
     auth: BasicAuth
 ) -> Result<HttpResponse> {
-    let mut mysql_pool = mysql_pool.lock().unwrap();
-    let mysql_pool = mysql_pool.as_mut().unwrap();
-
-    let redis_cli = redis_cli.lock().unwrap();
+    let mut cred_ctx = cred_ctx.lock().unwrap();
 
     if let Some(password) = auth.password() {
-        if verify(mysql_pool, &redis_cli, **redis_lifetime, &secret, auth.user_id(), password).await {
+        if verify(&mut cred_ctx, **redis_lifetime, &secret, auth.user_id(), password).await {
             Ok(HttpResponse::Ok().finish())
         } else {
             Ok(HttpResponse::Forbidden().finish())
@@ -100,33 +142,19 @@ async fn auth(
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
-    let redis_uri = get_enval("REDIS_URI", "redis://127.0.0.1/");
-    let redis_lifetime = get_enval("REDIS_LIFETIME", "3600");
-
-    let mysql_host = get_enval("MYSQL_HOST", "127.0.0.1");
-    let mysql_port = get_enval("MYSQL_PORT", "3306").parse::<u16>().unwrap();
-    let mysql_username = get_enval("MYSQL_USERNAME", "minauth"); // default for debug
-    let mysql_password = get_enval("MYSQL_PASSWORD", "minauth"); // default for debug
-    let mysql_dbname = get_enval("MYSQL_DBNAME", "minauth");
-    let mysql_opts = OptsBuilder::default()
-        .ip_or_hostname(mysql_host)
-        .tcp_port(mysql_port)
-        .user(Some(mysql_username))
-        .pass(Some(mysql_password))
-        .db_name(Some(mysql_dbname))
-        .into();
-    let mysql_pool = Data::new(Mutex::new(Some(Box::new(Pool::new::<Opts>(mysql_opts)))));
+    let cred_ctx = Data::new(Mutex::new(CredContext {
+        mysql: Some(new_mysql_pool()),
+        redis: Some(new_redis_client()),
+    }));
 
     let ret = {
-        let mysql_pool = Data::clone(&mysql_pool);
-        let redis_cli = Data::new(Mutex::new(redis::Client::open(redis_uri.as_str()).unwrap()));
-        let redis_lifetime = Data::new(redis_lifetime.parse::<usize>().unwrap());
+        let cred_ctx = Data::clone(&cred_ctx);
+        let redis_lifetime = Data::new(get_enval_usize("REDIS_LIFETIME", "3600"));
         let secret = Data::new(get_enval("PASSWORD_SECRET", ""));
 
         HttpServer::new(move || {
             App::new()
-                .app_data(Data::clone(&mysql_pool))
-                .app_data(Data::clone(&redis_cli))
+                .app_data(Data::clone(&cred_ctx))
                 .app_data(Data::clone(&redis_lifetime))
                 .app_data(Data::clone(&secret))
                 .service(auth)
@@ -136,10 +164,10 @@ async fn main() -> std::io::Result<()> {
             .await
     };
 
-    let mut mysql_pool = mysql_pool.lock().unwrap();
-    let mysql_pool = std::mem::replace(&mut *mysql_pool, None);
-    mysql_pool.unwrap().disconnect().await.unwrap();
-    info!("The connection pool was disconnected.");
+    let mut cred_ctx = cred_ctx.lock().unwrap();
+    cred_ctx.close().await;
+
+    info!("All connections was closed.");
 
     ret
 }
